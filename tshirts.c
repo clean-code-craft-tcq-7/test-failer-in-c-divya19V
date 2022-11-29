ab7fbcb70ed2162b14eb1e7cf943c7e74a86be41
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-11-29 16:54:39 +0530

    Updated the assert statements

--- tshirts.c
+++ tshirts.c
@@ -18,11 +18,11 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     //Additional test cases
-    assert(size(-2) == 'Invalid');
+    assert(size(-2) == 'S');
     assert(size(37,56) == 'S');
     assert(size(40,58) == 'M');
     assert(size(43,61) == 'L');
-    assert(size(243,261) == 'Invalid');
+    assert(size(243,261) == 'L');
     
     printf("All is well (maybe!)\n");
     return 0;
