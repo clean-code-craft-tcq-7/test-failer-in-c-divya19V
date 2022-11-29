f8f2e053af4626d64657a89940cfa15a39e475bd
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-11-29 18:36:07 +0530

    Update the assert statement

--- misaligned.c
+++ misaligned.c
@@ -4,10 +4,12 @@
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            assert(minorColor[i] == "Orange");
         }
     }
     return i * j;
