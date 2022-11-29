e004284ca29a7f059a8c8cde40e0598b52477cf4
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-11-29 18:37:02 +0530

    Added a asset statement in the loop

--- misaligned.c
+++ misaligned.c
@@ -9,7 +9,7 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            assert(minorColor[i] == "Orange");
+            assert(minorColor[i] == minorColor[j]);
         }
     }
     return i * j;
