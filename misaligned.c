a71d8b123ef11956a2011740460396bf8345ba0b
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-12-06 18:16:16 +0530

    Updated misalignment

--- misaligned.c
+++ misaligned.c
@@ -2,14 +2,21 @@
 #include <assert.h>
 
 //Method to test the printColorMap function
-/*void formatPrintfInputColorMap (int majorNumberIndex, int minorNumberIndex, const char* recvMajorColor, const char* recvMinorColor)
+void formatPrintfInputColorMap (int majorNumberIndex, int minorNumberIndex, const char* recvMajorColor, const char* recvMinorColor)
 {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-   
+    boolean isSingleDigit = true;
+    
+    //Test case to check for proper major and minor colors
     assert(majorColor[majorNumberIndex] == recvMajorColor);
     assert(minorColor[minorNumberIndex] == recvMinorColor);
-}*/
+    
+    isSingleDigit = (majorNumberIndex * minorNumberIndex) / 10;
+    
+    //Test case to test single digit and it fails as alignment is not right
+    assert(isSingleDigit, True);
+}
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -19,7 +26,7 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            //formatPrintfInputColorMap(i, j, majorColor[i], minorColor[i]);
+            formatPrintfInputColorMap(i, j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
